f96b401fdaa8144b9ecdebc6fbba0c0f384d83f6
heesung33 <chaeheesung33@gmail.com>
2022-03-15 14:38:50 +0900

    scanner

---
+++ Java/scanner/src/Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int a;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하세요 : ");
+
+        a = scan.nextInt();
+
+        System.out.println("입력된 숫자는 " + a + "입니다.");
+    }
+
+}
