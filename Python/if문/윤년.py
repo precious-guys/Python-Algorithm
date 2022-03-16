706d01d6cebbb8eaa6d447e24b95070571da5492
heesung33 <chaeheesung33@gmail.com>
2022-03-16 18:56:48 +0900

    📝윤년

---
+++ Python/if문/윤년.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+if a%4==0 and a%100!=0 or a%400==0 :
+    print("1")
+else :
+    print("0")
+
+
+#if a%4==0 :
+#   if a%100!=0 :
+#        print(1)
+
+#if a%400==0 :
+#   print(1)
+
+#if a%400!=0 and a%100==0 :
+#    print(0)
