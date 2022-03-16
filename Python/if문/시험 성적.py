8a71bc77e82c9689806dd71fcbb6e98960e7c75e
heesung33 <chaeheesung33@gmail.com>
2022-03-16 18:56:57 +0900

    📝시험 성적

--- Python/if문/시험 성적.py
+++ Python/if문/시험 성적.py
@@ -1,12 +1,12 @@
 a= int(input())
 
-if a>90:
+if 100>=a>89:
     print("A")
-elif a>80:
+elif a>79:
     print("B")
-elif a>70:
+elif a>69:
     print("C")
-elif a>60:
+elif a>59:
     print("D")
-else :
+elif a<60:
     print("F")
