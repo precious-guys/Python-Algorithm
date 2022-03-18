d54885ee2ad6c950a0a3e8541dd26d5e07a04bb3
heesung33 <chaeheesung33@gmail.com>
2022-03-18 10:29:54 +0900

    📝오븐시계

--- Python/if문/오븐 시계.py
+++ Python/if문/오븐 시계.py
@@ -1,10 +1,21 @@
-Hour, Minute = map(int, input().split)
+Hour, Minute = map(int, input().split())
 
 Add = int(input())
 
-if Minute+Add>59 :
-    if Hour>0 :
-        Hour += Add+Minute//60
-        Add+Minute -= Add 
+temp = int
+
+Minute += Add
+
+if Minute>59 :
+
+
+    Hour += (Minute)//60
+
+    Minute -= (Minute//60)*60
+    if Hour>23 :
+        Hour -= 24
+
+print(Hour, Minute)
+
 
     
